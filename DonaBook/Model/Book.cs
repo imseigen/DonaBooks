7675bb54dc72439885a21a832273e612ab349acc
Maulana Jidan Azizi <idanaf3@gmail.com>
2025-05-03 19:02:29 +0700

    Menambahkan class Book

---
+++ DonaBook/Model/Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaBook.Model
+{
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        ScienceFiction,
+        Fantasy,
+        Mystery,
+        Romance,
+        Horror,
+        Biography,
+        SelfHelp,
+        History,
+        Unknown
+    }
+
+    public enum Category
+    {
+        AnakAnak,
+        Remaja,
+        Dewasa,
+        Pendidikan,
+        Lainnya
+    }
+
+    public enum BookCondition
+    {
+        Baru,
+        BekasBaik,
+        BekasRusak
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+
+        public Genre Genre { get; set; }
+        public Category Category { get; set; }
+        public BookCondition Condition { get; set; }
+
+        public int Quantity { get; set; }
+        public bool IsVerified { get; set; }
+        public string? Review { get; set; }
+        public int Rating { get; set; }
+
+        // Table-driven rating default berdasarkan genre
+        private static readonly Dictionary<Genre, int> GenreDefaultRatings = new()
+        {
+            { Genre.Fiction, 5 },
+            { Genre.NonFiction, 4 },
+            { Genre.ScienceFiction, 4 },
+            { Genre.Fantasy, 5 },
+            { Genre.Mystery, 4 },
+            { Genre.Romance, 3 },
+            { Genre.Horror, 4 },
+            { Genre.Biography, 4 },
+            { Genre.SelfHelp, 3 },
+            { Genre.History, 4 }
+        };
+
+        public Book(string title, string publisher, Genre genre, string author, Category category, BookCondition condition, int quantity, int donorId)
+        {
+            Title = title;
+            Publisher = publisher;
+            Genre = genre;
+            Author = author;
+            Category = category;
+            Condition = condition;
+            Quantity = quantity;
+            IsVerified = false;
+
+            Rating = GenreDefaultRatings.ContainsKey(genre) ? GenreDefaultRatings[genre] : 0;
+        }
+    }
+}
--- DonaBook/obj/Debug/net8.0/DonaBook.AssemblyInfo.cs
+++ DonaBook/obj/Debug/net8.0/DonaBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DonaBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e38960b811ad26cd879d303424d6f2710fcb2289")]
 [assembly: System.Reflection.AssemblyProductAttribute("DonaBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DonaBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
